46ba29df86e6611f5b3029034f7ceebcc8fc9a0b
Suzanne Richman <suzanne@favoredfortune.com>
2018-02-28 20:31:10 -0800

    updated from today's lecture

---
+++ scripts/views/adminView.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var app = app || {};
+const __API_URL__ = 'http://localhost:3000';
+
+(function (module){
+  const adminView = [];
+
+adminView.initAdminPage = function(){
+  $('#admin-form').on('submit', function(event){
+    event.preventDefault();
+    let tokenEntered = event.target.passphrase.value;
+
+    $.get(`${__API_URL__}/admin`, {tokenEntered})
+      .then(//what to do next - if true then show buttons, if false do something else)
+      //then(function(verified){
+        // if verified true do something
+        //  localStorage.tokenVerified = true; 
+        // page ('/whereever')
+        // else do a different thing
+        // page('elesewhere')
+        // localStorage.tokenVerified = false; 
+//       })
+//       .catch(err)
+ 
+//     })
+//     next();
+// })
+
+// adminView.verify(function(ctx, next){
+//   if localStorage.token do something
+//   else do another thing
+// })
+
+
+  module.adminView = adminView;
+})(app)
--- scripts/views/bookView.js
+++ scripts/views/bookView.js
@@ -5,11 +5,38 @@ var app = app || {};
 (function (module) {
   const bookView = {};
 
+  function resetView(){
+    // @252PM
+  }
+
   bookView.initIndexPage = function () {
     $('.container').hide();
     $('#books').show();
     app.Book.all.map(book => $('#books').append(book.toHtml()));
   };
+
+  bookView.initDetailPage = function (ctx) {
+    resetView();
+    //at 251 in lecture
+    // compile handlebars template for detail view
+  //   $('.detail-view').show();
+  //   $('.detail-view-container').hide();
+  //   $('#books').show();
+  //   app.Book.all.map(book => $('#books').append(book.toHtml()));
+  // };
+
+  bookView.initFormPage = function () {
+    $('.container').hide();
+    $('.formView').show();
+
+    let createBook={
+      //take values from the form and add assign them to key value pairs
+    };
+
+
+    module.Book.createBook(createBook)
+  //   app.Book.all.map(book => $('#books').append(book.toHtml()));
+  // };
 
   bookView.handleMainNav = () => {
     $('.main-nav').on('click', '.tab', function() {
